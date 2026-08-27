31a33088596d95ef5d5dc5bde4e45a0910b5b2f0
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-08-27 23:13:34 +0530

    Updated app.js

--- Cors/src/app.js
+++ Cors/src/app.js
@@ -4,4 +4,9 @@ import cors from 'cors';
 const app = express();
 app.use(express.json());
 
+app.use(cors({
+    origin: "http://localhost:5173/",
+    credentials: true
+}));
+
 export default app;
