ec88f0b1d1dfd49b3612fb2b6eef2337aebffdd7
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-08-28 17:35:22 +0530

    Updated multer middleware

--- Multer-And-Imagekit/src/middlewares/multer.middleware.js
+++ Multer-And-Imagekit/src/middlewares/multer.middleware.js
@@ -1,10 +1,21 @@
+// Import Multer so we can handle file uploads (39)
 import multer from "multer";
 
+// Create a Multer instance with our upload configuration (40)
 const upload = multer({
+
+    // Store uploaded files temporarily in memory (RAM) instead of saving them locally (41)
     storage: multer.memoryStorage(),
+
+    // Set limits for uploaded files (42)
     limits: {
+
+        // Allow a maximum file size of 5 MB (43)
         fileSize: 5 * 1024 * 1024
+
     }
+
 });
 
+// Export the configured Multer middleware so routes can use it (15)
 export default upload;
