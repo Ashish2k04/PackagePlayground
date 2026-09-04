5fe9e931ef18d5f5f6372c4299f9d2cf3877a68d
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-04 20:58:54 +0530

    Added array for tempopary conversation history for ai to remember.

--- Langchain-readline-terminal-based-chatbot/index.js
+++ Langchain-readline-terminal-based-chatbot/index.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { ChatGroq } from "@langchain/groq";
 import readline from "readline/promises";
+import { HumanMessage } from 'langchain';
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -12,16 +13,21 @@ const model = new ChatGroq({
     temperature: 0
 });
 
+let messages = []
+
 while (true) {
     const userInput = await rl.question("\x1b[32mWhat's in your mind? 😊:\x1b[0m ");
+
+    messages.push(new HumanMessage(userInput));
 
     if (userInput.toLowerCase() === "exit") {
         break;
     }
 
     try {
-        const response = await model.invoke(userInput);
+        const response = await model.invoke(messages);
         console.log("\x1b[36mAI CHATBOT 🤖:\x1b[0m", response.text);
+        messages.push(response);
     } catch (error) {
         console.error("Error:", error.message);
     }
