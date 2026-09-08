457d546a4d2b18723c8ce5ace85287b7a2031367
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-08 19:19:10 +0530

    Imported authRouter in app.js and used

--- Error-Middleware-Not-a-package/src/app.js
+++ Error-Middleware-Not-a-package/src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import { handleError } from './middlewares/error.middleware.js';
+import authRouter from './routes/auth.route.js';
 
 const app = express();
 app.use(express.json());
+
+app.use('/api', authRouter);
 
 app.use(handleError);
 export default app;
