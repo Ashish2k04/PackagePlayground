304c37433dc73ba3874a561dae0fa3f05e6e8532
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-08 19:14:26 +0530

    Imported register controller and used inside auth routes

--- Error-Middleware-Not-a-package/src/routes/auth.route.js
+++ Error-Middleware-Not-a-package/src/routes/auth.route.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import registerUserController from "../controllers/auth.controller.js";
 
 const authRouter = Router();
+
+authRouter.post('/register', registerUserController);
 
 export default authRouter;
