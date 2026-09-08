6bc9667e93fc9447fe41e594c869b26b5f9ce342
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-08 18:48:35 +0530

    Imported and used handleError function inside app.js

--- Error-Middleware-Not-a-package/src/app.js
+++ Error-Middleware-Not-a-package/src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import { handleError } from './middlewares/error.middleware.js';
 
 const app = express();
 app.use(express.json());
 
+app.use(handleError);
 export default app;
