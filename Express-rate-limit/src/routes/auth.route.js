a7403d04339941aee17bd3fa4587eed947a42aae
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-11 21:18:58 +0530

    Imported loginController inside auth router

--- Express-rate-limit/src/routes/auth.route.js
+++ Express-rate-limit/src/routes/auth.route.js
@@ -1,5 +1,8 @@
 import {Router} from 'express';
+import loginController from '../controllers/auth.controller.js';
 
 const appRouter = Router();
+
+appRouter.post('/login', loginController);
 
 export default appRouter;
