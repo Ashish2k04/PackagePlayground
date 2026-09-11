761249b9c040fb478233b05f118ee2a968ce1f15
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-11 21:20:21 +0530

    Imported appRouter inside app.js

--- Express-rate-limit/src/app.js
+++ Express-rate-limit/src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
+import appRouter from "./routes/auth.route.js";
 
 const app = express();
 app.use(express.json());
+
+app.use('/api', appRouter);
 
 export default app;
