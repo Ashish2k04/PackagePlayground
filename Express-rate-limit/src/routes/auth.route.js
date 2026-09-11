2f7bd6cbd01d754a63e8127cd5fda5c934e98cde
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-11 21:27:30 +0530

    Added loginMiddelware in login route api

--- Express-rate-limit/src/routes/auth.route.js
+++ Express-rate-limit/src/routes/auth.route.js
@@ -1,8 +1,9 @@
 import {Router} from 'express';
 import loginController from '../controllers/auth.controller.js';
+import { loginLimiter } from '../middlewares/rateLimit.middlware.js';
 
 const appRouter = Router();
 
-appRouter.post('/login', loginController);
+appRouter.post('/login', loginLimiter, loginController);
 
 export default appRouter;
