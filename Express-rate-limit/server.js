17a10de5a1f71873e056583e7e8f5d69f0e03470
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-11 21:41:57 +0530

    Comments added inside server.js for future guidance

--- Express-rate-limit/server.js
+++ Express-rate-limit/server.js
@@ -1,5 +1,9 @@
+// Import the Express application from app.js (1)
 import app from "./src/app.js";
 
+// Start the Express server on port 3000 (2)
 app.listen(3000, ()=>{
+
+    // Display a message when the server starts successfully (3)
     console.log('Server is running on port 3000')
 })
