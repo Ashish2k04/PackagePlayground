a34c64690f9e13542acc04c25a63c9056995f849
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-11 21:39:45 +0530

    Comments added inside rateLimit middleware for future guidance

--- Express-rate-limit/src/middlewares/rateLimit.middlware.js
+++ Express-rate-limit/src/middlewares/rateLimit.middlware.js
@@ -1,7 +1,18 @@
+// Import rateLimit from express-rate-limit to limit incoming requests (1)
 import {rateLimit} from 'express-rate-limit';
 
+// Create a rate limiter for the login API (2)
 export const loginLimiter = rateLimit({
+
+    // Define the time window in which requests are counted (3)
+    // 1 * 60 * 1000 = 1 minute
     windowMs: 1 * 60 * 1000,
+
+    // Allow a maximum of 5 requests from the same client
+    // during the defined time window (4)
     limit: 5,
+
+    // Message returned when the request limit is exceeded (5)
     message: "Too many requests, please try again later."
+
 })
