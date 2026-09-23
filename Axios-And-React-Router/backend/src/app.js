35ddb6bb31331b6aae816c09133726660f9a3d82
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-23 23:35:21 +0530

    Cors added inside app.js

--- Axios-And-React-Router/backend/src/app.js
+++ Axios-And-React-Router/backend/src/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import noteRouter from './routes/note.route.js';
 import handleError from './middlewares/error.middleware.js';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["post", "delete"]
+}))
 
 app.use('/api', noteRouter);
 
