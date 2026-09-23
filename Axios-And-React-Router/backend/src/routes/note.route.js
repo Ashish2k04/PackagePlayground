6cc21aab5692002e11c39ee0c9fb9463367be3fb
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-23 22:43:57 +0530

    Controller name updated

--- Axios-And-React-Router/backend/src/routes/note.route.js
+++ Axios-And-React-Router/backend/src/routes/note.route.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { noteController } from "../controllers/notes.controller.js";
+import { createNotes } from "../controllers/notes.controller.js";
 
 const noteRouter = Router();
 
-noteRouter.post('/create-note', noteController);
+noteRouter.post('/create-note', createNotes);
 
 export default noteRouter;
